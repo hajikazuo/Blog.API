2519019eb50d270767d79ad1f3b432ee8f204da2
hajikazuo <hajikazuo@outlook.com>
2024-08-29 17:53:39 -0300

    Add project files.

---
+++ Blog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.API.Data;
+using Blog.API.Models.Domain;
+using Blog.API.Models.DTO;
+using Blog.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RT.Comb;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly ICombProvider _comb;
+
+        public CategoriesController(ICategoryRepository categoryRepository, ICombProvider comb)
+        {
+            _categoryRepository = categoryRepository;   
+            _comb = comb;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Id = _comb.Create(),
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _categoryRepository.CreateAsync(category);
+          
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Blog.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Blog.API.Data;
+using Blog.API.Models.Domain;
+using Blog.API.Repositories.Interface;
+
+namespace Blog.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
